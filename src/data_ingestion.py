8dfed308ca5303a2091a2a79e7fd1bec1df63405
kennedylsodre <klacerda88@gmail.com>
2025-03-23 14:10:40 -0300

    Code

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,2 +1,20 @@
 #%%
-import pandas as pd
+import kagglehub
+import os 
+import shutil
+from google.cloud import storage
+#%%
+
+# Download latest version
+path_download = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+path_storage = '../data'
+
+def download_data(path_download,path_storage): 
+    os.makedirs(path_storage,exist_ok=True) 
+    for file in os.listdir(path_download):
+        shutil.move(os.path.join(path_download,file),os.path.join(path_storage,file)) 
+
+#%% 
+#Send files for buckt in GCP 
+
+client = storage.Client()        
